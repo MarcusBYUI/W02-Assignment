deec996dd7b3580dbddcb887597e4ebb5dc58538
MarcusBYUI <ojo18002@byui.edu>
2022-09-29 10:16:37 +0100

    completed message task

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
   templateUrl: './message-edit.component.html',
-  styleUrls: ['./message-edit.component.css']
+  styleUrls: ['./message-edit.component.css'],
 })
 export class MessageEditComponent implements OnInit {
+  currentSender: string = 'Marcus';
 
-  constructor() { }
+  @ViewChild('subject', { static: true }) subjectRef: ElementRef;
+  @ViewChild('msgText', { static: true }) msgTextRef: ElementRef;
 
-  ngOnInit(): void {
+  @Output('addMessageEvent') addMessage: EventEmitter<Message> =
+    new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSendMessage(e: Event) {
+    e.preventDefault();
+    this.addMessage.emit(
+      new Message(
+        1,
+        this.subjectRef.nativeElement.value,
+        this.msgTextRef.nativeElement.value,
+        this.currentSender
+      )
+    );
   }
 
+  onClear() {
+    this.subjectRef.nativeElement.value = '';
+    this.msgTextRef.nativeElement.value = '';
+  }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
   templateUrl: './message-list.component.html',
-  styleUrls: ['./message-list.component.css']
+  styleUrls: ['./message-list.component.css'],
 })
 export class MessageListComponent implements OnInit {
+  messages: Message[] = [
+    new Message(1, 'greetings', 'Hello Marcus', 'Luke'),
+    new Message(2, 'greetings', 'Hi, How are you doing', 'Marcus'),
+    new Message(3, 'greetings', 'I am good, just about coming to you', 'Luke'),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
   }
-
 }
