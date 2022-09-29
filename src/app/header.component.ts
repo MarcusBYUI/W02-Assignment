1b2b027918168826a8090882719b037c8a75b8a4
MarcusBYUI <ojo18002@byui.edu>
2022-09-29 09:39:25 +0100

    completed header selection task

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'cms-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output('selectedFeatureEvent') selectedFeature: EventEmitter<string> =
+    new EventEmitter();
   constructor() {}
+
+  onSelected(selectedEvent: string) {
+    this.selectedFeature.emit(selectedEvent);
+  }
 
   ngOnInit(): void {}
 }
