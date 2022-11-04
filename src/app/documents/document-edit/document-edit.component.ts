112eacf25824ee26d01987a169319a8d83f5da03
MarcusBYUI <ojo18002@byui.edu>
2022-11-04 17:18:07 +0100

    completed document task

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'cms-document-edit',
   templateUrl: './document-edit.component.html',
-  styleUrls: ['./document-edit.component.css']
+  styleUrls: ['./document-edit.component.css'],
 })
 export class DocumentEditComponent implements OnInit {
-
-  constructor() { }
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
+  constructor(
+    private docService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      let id = params['id'];
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalDocument = this.docService.getDocument(id);
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
   }
 
+  onCancel() {
+    this.router.navigateByUrl('/documents');
+  }
+
+  onSubmit(form: NgForm) {
+    let value = form.value; // get values from form’s fields
+    //set id to prev doc or get new id based on edit mode
+    let id: string = this.editMode
+      ? this.originalDocument.id
+      : String(this.docService.getMaxId() + 1);
+
+    let newDocument = new Document(
+      id,
+      value.title,
+      value.description,
+      value.url
+    );
+
+    if (this.editMode) {
+      //   documentService.updateDocument(originalDocument, newDocument)
+      this.docService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      //   documentService.addDocument(newDocument)
+      this.docService.addDocument(newDocument);
+    }
+    //  route back to the '/documents' URL
+    this.router.navigateByUrl('/documents');
+  }
 }
